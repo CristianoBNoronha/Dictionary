f582c63a1f03ec7cb2a75fb69e27eee1ab99ff4e
CristianoBNoronha <cris_nor@hotmail.com>
2022-08-09 14:54:31 -0300

    Add files

---
+++ ExerciseDictionary/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExerciseDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> votes = new Dictionary<string, int>();
+            Console.Write("Enter full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int number = int.Parse(line[1]);
+                        if (votes.ContainsKey(name))
+                        {
+                            int soma = votes[name];
+                            votes[name] = soma + number;
+                        }
+                        else
+                        {
+                            votes.Add(name, number);
+                        }
+                    }
+                }
+                foreach (KeyValuePair<string, int> item in votes)
+                {
+                    Console.WriteLine(item.Key + " : " + item.Value);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Dictionary
 Exercise following C# course instructions, to learn about Dictionary and file streaming.
+
+<a>
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg" width="60" height="60"/>
+<a/>
